2c23293da76ad32a5c88e29d8b1a7bc6bd1c910e
Remco Bouckaert <higgs.ml@gmail.com>
2017-06-15 15:36:17 +1200

    start TMA Beauti template

---
+++ src/beast/evolution/likelihood/FixedTreeLikelihood.java
@@ -0,0 +1,9 @@
+package beast.evolution.likelihood;
+
+import beast.core.Description;
+
+@Description("Dummy treeLikelihood for a fixed tree that provides a partition so it integrates "
+		+ "with BEAUti")
+public class FixedTreeLikelihood extends GenericTreeLikelihood {
+
+}
